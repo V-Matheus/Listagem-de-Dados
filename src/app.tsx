be1367f5628a79b2b63c324add4aac78009c55c5
Victor Matheus <victormatheus507@gmail.com>
2024-02-20 22:27:14 -0300

    primeiros estilos

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,23 @@
+import { Plus } from "lucide-react"
+
 function App() {
 
   return (
-      <h1>oi</h1>
+      <div className="py-10 space-y-8">
+        <div>
+          header
+          tabs 
+        </div>
+        <main className="max-w-6xl mx-auto space-y-5">
+          <div className="flex items-center gap-3">
+            <h1 className="text-xl font-bold">Tags</h1>
+            <button className="inline-flex items-center gap-1.5 text-xl bg-teal-300 text-teal-950 font-medium rounded-full px-2 py-1">
+              <Plus className="size-3"/>            
+              Create New
+              </button>
+          </div>
+        </main>
+      </div>
   )
 }
 
